d2102c2b6dc11cfd0d70d78a050302652a6d6910
Josh <joshua.croke.2021@uni.strath.ac.uk>
2023-03-05 17:15:24 +0000

    Now works with phone rotation to track 3d space

--- App.js
+++ App.js
@@ -1,35 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Gyroscope, DeviceMotion  } from 'expo-sensors';
-import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Gyroscope, DeviceMotion } from "expo-sensors";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
 
 export default function App() {
-
   const [{ x, y, z }, setData] = useState({
     x: 0,
     y: 0,
     z: 0,
   });
+
+  const [{ alpha, beta, gamma }, setRotation] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+
   const [subscription, setSubscription] = useState(null);
-  const [motiondata, setmotiondata] = useState(null);
+  const [motionData, setMotionData] = useState(null);
 
   const _slow = () => Gyroscope.setUpdateInterval(1000); //Change for updating frequncy
-  const _fast = () => Gyroscope.setUpdateInterval(16);   //Change for updating frequncy
-  DeviceMotion.setUpdateInterval(10000); //Change for printing frequncy
+  const _fast = () => Gyroscope.setUpdateInterval(16); //Change for updating frequncy
+  DeviceMotion.setUpdateInterval(100); //Change for printing frequncy
 
   const _subscribe = () => {
     setSubscription(
-      Gyroscope.addListener(gyroscopeData => {
+      Gyroscope.addListener((gyroscopeData) => {
         setData(gyroscopeData);
       })
     );
     setSubscription(
-      DeviceMotion.addListener(motionData => {
-        console.log(motionData);
-        setmotiondata(motionData);
+      DeviceMotion.addListener((motionData) => {
+        // console.log(motionData);
+        setMotionData(motionData);
+        setRotation(motionData.rotation);
       })
     );
   };
+
+  //https://www.w3resource.com/javascript-exercises/javascript-math-exercise-34.php
+  function radians_to_degrees(radians) {
+    const pi = Math.PI;
+    return radians * (180 / pi);
+  }
 
   const _unsubscribe = () => {
     subscription && subscription.remove();
@@ -41,19 +54,30 @@ export default function App() {
     return () => _unsubscribe();
   }, []);
 
-
   return (
     <View style={styles.container}>
       {/* <Text style={styles.text}> motion: {motiondata}</Text> */}
-      <Text style={styles.text}>Gyroscope:</Text>
-      <Text style={styles.text}>x: {x}</Text>
-      <Text style={styles.text}>y: {y}</Text>
-      <Text style={styles.text}>z: {z}</Text>
+      <Text style={styles.text}>Rotation:</Text>
+      <Text style={styles.text}>
+        x: {Math.round(radians_to_degrees(alpha) + 180)}
+      </Text>
+      <Text style={styles.text}>
+        y: {Math.round(radians_to_degrees(beta) + 180)}
+      </Text>
+      <Text style={styles.text}>
+        z: {Math.round(radians_to_degrees(gamma) + 180)}
+      </Text>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
-          <Text>{subscription ? 'On' : 'Off'}</Text>
+        <TouchableOpacity
+          onPress={subscription ? _unsubscribe : _subscribe}
+          style={styles.button}
+        >
+          <Text>{subscription ? "On" : "Off"}</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+        <TouchableOpacity
+          onPress={_slow}
+          style={[styles.button, styles.middleButton]}
+        >
           <Text>Slow</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={_fast} style={styles.button}>
@@ -69,27 +93,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingVertical: 12,
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   text: {
     fontSize: 16,
     lineHeight: 21,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.25,
-    color: 'white',
+    color: "white",
   },
-  buttonContainer:{
-    color: 'white',
+  buttonContainer: {
+    color: "white",
   },
 });
